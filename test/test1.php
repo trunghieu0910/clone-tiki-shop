fc8c2c729f20ef78ee32353eb8efaf01eadf3bf7
trung hieu <trunghieuuazz@gmail.com>
2023-07-01 16:19:18 +0700

    test conflict

--- test/test1.php
+++ test/test1.php
@@ -21,7 +21,7 @@
     <input class="form-control txt" style="" type="search" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Search" aria-label="Search">
     <button class="btn btn-outline-success my-2 my-sm-0 border-0" type="submit">Search</button>
   </form>
-  
+  <a href=""></a>
   </div>
   
 
