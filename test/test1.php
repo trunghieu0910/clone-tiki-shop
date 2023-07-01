cd7247d0e78222c0c63aaee81d2f102dc318e4c3
trung hieu <trunghieuuazz@gmail.com>
2023-07-01 10:29:56 +0700

    Add new project clone-tiki-shop

---
+++ test/test1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Button Example</title>
+  <!-- Nạp Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link rel="stylesheet" href="test.css">
+</head>
+<body>
+
+  <!-- Nút Button -->
+  <button class="btn btn-success">Click Me</button>
+  <p class="text-black-50">hello</p>
+  <p class="text-gray-100">hello world</p>
+  <div class="bg-search">
+  <div class="with-search"></div>
+  <i class="iconSearch fas fa-search-plus"></i>
+  <form class="form-inline" style="with: 200px;">
+    <input class="form-control txt" style="" type="search" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0 border-0" type="submit">Search</button>
+  </form>
+  
+  </div>
+  
+
+  <!-- Nạp thư viện jQuery -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+  <!-- Nạp thư viện Popper.js -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <!-- Nạp Bootstrap JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+</body>
+</html>
