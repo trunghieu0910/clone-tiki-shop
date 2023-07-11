f851fa6e3a8585c99b2eef54a90b2d88e1a7b8d9
trunghieu <trunghieuuazz@gmail.com>
2023-07-12 00:29:14 +0700

    push test

--- test/test1.php
+++ test/test1.php
@@ -9,21 +9,24 @@
   <link rel="stylesheet" href="test.css">
 </head>
 <body>
-
-  <!-- Nút Button -->
-  <button class="btn btn-success">Click Me</button>
-  <p class="text-black-50">hello</p>
-  <p class="text-gray-100">hello world</p>
-  <div class="bg-search">
-  <div class="with-search"></div>
-  <i class="iconSearch fas fa-search-plus"></i>
-  <form class="form-inline" style="with: 200px;">
-    <input class="form-control txt" style="" type="search" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0 border-0" type="submit">Search</button>
-  </form>
-  <p>thắng </p>
-  <p>abcd</p>
+  <div class="w-40 h-12 background-custom">123456789</div>
+  
+  <div class="input-group has-validation">
+  <span class="input-group-text">@</span>
+  <div class="form-floating is-invalid">
+    <input type="text" class="form-control is-invalid" id="floatingInputGroup2" placeholder="Username" required>
+    <label for="floatingInputGroup2">Username</label>
   </div>
+  <div class="invalid-feedback">
+    Please choose a username.
+  </div>
+</div>
+<select class="form-select bg-gray-100" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
   
 
   <!-- Nạp thư viện jQuery -->
